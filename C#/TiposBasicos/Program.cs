b3cdae54699a4a7622dbd1e8c646bee3f7acfbfd
Notim_ <paulino.joaovitor@yahoo.com.br>
2018-12-13 23:38:29 -0200

    Up

---
+++ C#/TiposBasicos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposBasicos
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        
+            // explicit native types
+            char charExample;
+            bool boolExample;
+            
+            
+            
+            
+        }
+    }
+}
